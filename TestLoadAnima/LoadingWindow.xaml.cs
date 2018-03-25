f781eb0a3c497cdc77c3b29da87d3a4bc9ca1869
Илья Остроух <ilya.ostrouh@yandex.ru>
2018-03-25 13:10:36 +0300

    Проба анимации ожидания (загрузки)

---
+++ TestLoadAnima/LoadingWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestLoadAnima
+{
+    /// <summary>
+    /// Логика взаимодействия для LoadingWindow.xaml
+    /// </summary>
+    public partial class LoadingWindow : Window, IDisposable
+    {
+        public Action Worker { get; set; }
+        public LoadingWindow(Action worker)
+        {
+            
+            InitializeComponent();
+            Worker = worker ?? throw new ArgumentNullException();
+        }
+
+        private void Load_Loaded(object sender, RoutedEventArgs e)
+        {
+            Task.Factory.StartNew(Worker)
+                .ContinueWith(t => { Close(); }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        public void Dispose()
+        {
+            
+        }
+    }
+}
---
+++ TestLoadAnima/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestLoadAnima
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        BlurEffect myEffect=new BlurEffect();
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            myEffect.Radius = 10;
+            Effect = myEffect;
+            using (LoadingWindow lw = new LoadingWindow(Simulator))
+            {
+                lw.Owner = this;
+                lw.ShowDialog();
+            }
+
+            myEffect.Radius = 0;
+            Effect = myEffect;
+        }
+
+        void Simulator()
+        {
+            for (int i = 0; i < 500; i++)
+            {
+                Thread.Sleep(5);
+            }
+        }
+    }
+}
